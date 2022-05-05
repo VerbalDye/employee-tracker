8fdfaaa1ba5552e43f3d35c7243987660f727bd7
Dustin Huntsberger <dhunts258@gmail.com>
2022-05-05 16:57:29 -0400

    added employees by manager and department methods

--- app.js
+++ app.js
@@ -1,14 +1,14 @@
 const inquirer = require('inquirer');
-const { getDepartments, addDepartment } = require('./lib/departments');
+const { getDepartments, getEmployeesByDepartment, addDepartment } = require('./lib/departments');
 const { getRoles, addRole } = require('./lib/roles');
-const { getEmployees, addEmployee, updateEmployeeRole, updateEmployeeManager } = require('./lib/employees')
+const { getEmployees, addEmployee, updateEmployeeRole, updateEmployeeManager, getEmployeesByManager } = require('./lib/employees')
 
 function functionSelection() {
     return inquirer.prompt({
         type: 'list',
         name: 'selection',
         message: 'What would you like to do?',
-        choices: ['View All Departments', 'View All Roles', 'View All Employees', 'Add a Department', 'Add a Role', 'Add an Employee', 'Update Employee Role', 'Update Employee Manager', 'Exit']
+        choices: ['View All Departments', 'View All Roles', 'View All Employees', 'View Employees By Manager', 'View Employees By Department', 'Add a Department', 'Add a Role', 'Add an Employee', 'Update Employee Role', 'Update Employee Manager', 'Exit']
     })
 }
 
@@ -20,6 +20,10 @@ function selectionLogic({ selection }) {
             return getRoles();
         case 'View All Employees':
             return getEmployees();
+        case 'View Employees By Manager':
+            return getEmployeesByManager();
+        case 'View Employees By Department':
+            return getEmployeesByDepartment();
         case 'Add a Department':
             return addDepartment();
         case 'Add a Role':
