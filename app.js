368350f3ecdc012093c237a88c2606ecc03ab916
VerbalDye <dhunts258@gmail.com>
2022-05-04 20:00:59 -0400

    added more methods

--- app.js
+++ app.js
@@ -1,36 +1,48 @@
 const inquirer = require('inquirer');
 const { getDepartments, addDepartment } = require('./lib/departments');
 const { getRoles, addRole } = require('./lib/roles');
-const { getEmployees, addEmployee, updateEmployee } = require('./lib/employees')
+const { getEmployees, addEmployee, updateEmployeeRole, updateEmployeeManager } = require('./lib/employees')
 
-inquirer.prompt({
-    type: 'list',
-    name: 'selection',
-    message: 'What would you like to do?',
-    choices: ['View All Departments', 'View All Roles', 'View All Employees', 'Add a Department', 'Add a Role', 'Add an Employee', 'Update an Employee Role']
-})
-    .then(({ selection }) => {
-        switch (selection) {
-            case 'View All Departments':
-                getDepartments();
-                break;
-            case 'View All Roles':
-                getRoles();
-                break;
-            case 'View All Employees':
-                getEmployees();
-                break;
-            case 'Add a Department':
-                addDepartment();
-                break;
-            case 'Add a Role':
-                addRole();
-                break;
-            case 'Add an Employee':
-                addEmployee();
-                break;
-            case 'Update an Employee Role':
-                updateEmployee();
-                break;
-        }
-    });
+function functionSelection() {
+    inquirer.prompt({
+        type: 'list',
+        name: 'selection',
+        message: 'What would you like to do?',
+        choices: ['View All Departments', 'View All Roles', 'View All Employees', 'Add a Department', 'Add a Role', 'Add an Employee', 'Update Employee Role', 'Update Employee Manager', 'Exit']
+    })
+        .then(({ selection }) => {
+            switch (selection) {
+                case 'View All Departments':
+                    getDepartments();
+                    break;
+                case 'View All Roles':
+                    getRoles();
+                    break;
+                case 'View All Employees':
+                    getEmployees();
+                    break;
+                case 'Add a Department':
+                    addDepartment();
+                    break;
+                case 'Add a Role':
+                    addRole();
+                    break;
+                case 'Add an Employee':
+                    addEmployee();
+                    break;
+                case 'Update Employee Role':
+                    updateEmployeeRole();
+                    break;
+                case 'Update Employee Manger':
+                    updateEmployeeManager();
+                    break;
+                case 'Exit':
+                    process.exit(1);
+            }
+        })
+        .then( () => {
+            functionSelection();
+        });
+}
+
+functionSelection();
