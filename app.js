7e2688ee308a46c0c5f89916375bee22ee5cb978
VerbalDye <dhunts258@gmail.com>
2022-05-05 21:22:51 -0400

    added delete functions and budget calcualtor

--- app.js
+++ app.js
@@ -1,14 +1,14 @@
 const inquirer = require('inquirer');
-const { getDepartments, getEmployeesByDepartment, addDepartment } = require('./lib/departments');
-const { getRoles, addRole } = require('./lib/roles');
-const { getEmployees, addEmployee, updateEmployeeRole, updateEmployeeManager, getEmployeesByManager } = require('./lib/employees')
+const { getDepartments, getEmployeesByDepartment, addDepartment, deleteDepartment, getDepartmentBudgets } = require('./lib/departments');
+const { getRoles, addRole, deleteRole } = require('./lib/roles');
+const { getEmployees, addEmployee, deleteEmployee, updateEmployeeRole, updateEmployeeManager, getEmployeesByManager } = require('./lib/employees')
 
 function functionSelection() {
     return inquirer.prompt({
         type: 'list',
         name: 'selection',
         message: 'What would you like to do?',
-        choices: ['View All Departments', 'View All Roles', 'View All Employees', 'View Employees By Manager', 'View Employees By Department', 'Add a Department', 'Add a Role', 'Add an Employee', 'Update Employee Role', 'Update Employee Manager', 'Exit']
+        choices: ['View All Departments', 'View All Roles', 'View All Employees', 'View Employees By Manager', 'View Employees By Department', 'Add a Department', 'Add a Role', 'Add an Employee', 'Delete a Department', 'Delete a Role', 'Delete an Employee', 'Update Employee Role', 'Update Employee Manager', 'Get Budget by Department', 'Exit']
     })
 }
 
@@ -30,10 +30,18 @@ function selectionLogic({ selection }) {
             return addRole();
         case 'Add an Employee':
             return addEmployee();
+        case 'Delete a Department':
+            return deleteDepartment();
+        case 'Delete a Role':
+            return deleteRole();
+        case 'Delete an Employee':
+            return deleteEmployee();
         case 'Update Employee Role':
             return updateEmployeeRole();
         case 'Update Employee Manager':
             return updateEmployeeManager();
+        case 'Get Budget by Department':
+            return getDepartmentBudgets();
         case 'Exit':
             process.exit(1);
     }
